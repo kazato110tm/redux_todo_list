6c6fff19443322724bcbeb0878bb2a3fa05b30a1
deguchi <deguchi.k.110@gmail.com>
2021-05-06 00:53:55 +0900

    reduxTodoList

---
+++ src/components/TodoApp.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default function TodoApp({ task, tasks, inputTask, addTask, resetTask }) {
+  return (
+    <div>
+      <input type="text" onChange={(e) => inputTask(e.target.value)} />
+      <input type="button" value="add" onClick={() => addTask(task)} />
+      <input type="button" value="reset" onClick={() => resetTask()} />
+      <ul>
+        {
+          tasks.map(function (item, i) {
+            return (
+              <li key={i}>{item}</li>
+            );
+          })
+        }
+      </ul>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { render } from 'react-dom';
+import tasksReducer from './reducers/tasks';
+import TodoApp from './containers/TodoApp';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const store = createStore(tasksReducer);
+
+render(
+  <Provider store={store}>
+    <TodoApp />
+  </Provider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
